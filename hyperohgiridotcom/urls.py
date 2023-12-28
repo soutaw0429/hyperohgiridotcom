9ca6804d1a43c4e17d88bb991f5a87dfa14c9c56
Sota Wakamatsu <soutaw0429@gmail.com>
2023-12-28 06:16:35 -0600

    Initial Commit: Create Home&About&Contact and implement login feature

---
+++ hyperohgiridotcom/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name="index"),
+    path('about', views.about, name="about"),
+    path('contact', views.contact, name="contact"),
+
+    #Django auth
+    path('accounts/login', auth_views.LoginView.as_view(template_name="accounts/login.html"), name='login')
+]
